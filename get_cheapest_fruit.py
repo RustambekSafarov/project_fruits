d1eea066d64d9dcbdb392ebe1a088a64a73bb118
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-12 15:59:24 +0500

    done

--- get_cheapest_fruit.py
+++ get_cheapest_fruit.py
@@ -8,12 +8,11 @@ def get_cheapest_fruit(data:str)->str:
         name of the cheapest fruit
     """
     list_of_fruits = data.split()[1:]
-    e = 0.0
+    e = int(list_of_fruits[0][list_of_fruits[0].index(',')+1:])
     for i in list_of_fruits:
         p = i[i.index(',')+1:]
         if e > float(p):
             e = float(p)
-
     return e
 
 print(get_cheapest_fruit(open('fruits.csv').read()))
